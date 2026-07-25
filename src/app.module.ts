4af9adebe880b4cd8983ce16111ef7849d2f628c
leobourlot <leobourlot@gmail.com>
2026-07-25 17:12:52 -0300

    cors a app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
+import cors from 'cors';
 import { AuthModule } from './auth/auth.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { CanchasModule } from './canchas/canchas.module';
@@ -35,6 +36,27 @@ import { ClubMiddleware } from './common/middleware/club.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cors({
+          origin: (origin, callback) => {
+            const allowedPatterns = [
+              /^https?:\/\/.*\.turnos\.bourderweb\.com\.ar$/,
+              /^https?:\/\/turnos\.bourderweb\.com\.ar$/,
+              /^http:\/\/localhost:\d+$/,
+              /^http:\/\/127\.0\.0\.1:\d+$/,
+              /^https:\/\/slateblue-locust-897822\.hostingersite\.com$/,
+            ];
+            if (!origin || allowedPatterns.some((p) => p.test(origin))) {
+              callback(null, true);
+            } else {
+              callback(new Error('No permitido por CORS'));
+            }
+          },
+          credentials: true,
+        }),
+      )
+      .forRoutes('*');
     // Aplicar el middleware a todas las rutas
     consumer.apply(ClubMiddleware).forRoutes('*');
   }
